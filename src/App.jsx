cdabf15d7e650076cc9b3223a79f47d33b70ce1e
raqueldfrutos <raqueldfrutos@gmail.com>
2025-12-09 16:20:17 +0100

    Delete note funtionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import { useState } from "react";
 
 const App = () => {
   const [notes, setNotes] = useState([]); // Global state - because it gonna be used in multiple components. When we submit a note it will be added to this notes array
+  const deleteNote = (id) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
+    if (confirmDelete) {
+      setNotes(notes.filter((note) => note.id !== id)); //this filters out the note we want to delete
+    }
+  };
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-4 text-center">📝Notes App</h2>
       <NoteForm notes={notes} setNotes={setNotes} />
-      <NoteList notes={notes} />
+      <NoteList notes={notes} deleteNote={deleteNote} />
     </div>
   );
 };
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,4 +1,4 @@
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
     return <p className="text-center text-gray-500">No Notes Yet</p>;
   }
@@ -20,6 +20,14 @@ const NoteList = ({ notes }) => {
             {note.priority}
           </p>
           <p className="mt-2">{note.description}</p>
+          <button
+            onClick={() => {
+              deleteNote(note.id);
+            }}
+            className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
+          >
+            🗑️Delete
+          </button>
         </div>
       ))}
     </div>
